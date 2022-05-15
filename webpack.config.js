bdd1bfbd30cd804c1c6d07e6c315968ab5ca4ec7
SayudhaStudio <mazaya.dev@gmail.com>
2022-05-15 22:02:45 +0700

    + Setup asset/resource

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,17 @@ const path = require('path');
 module.exports = {
 	entry : './src/index.js',
 	output: {
-		filename: 'bundle.js',
-		path    : path.resolve(__dirname, './dist'),
+		filename  : 'bundle.js',
+		path      : path.resolve(__dirname, './dist'),
+		publicPath: 'dist/',
 	},
 	mode  : 'none',
+	module: {
+		rules: [
+			{
+				test: /\.(png|jpg)$/,
+				type: 'asset/resource',
+			},
+		],
+	},
 };
