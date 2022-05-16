35e0c982e8a20797f557de6e62d71586aadf8de7
SayudhaStudio <mazaya.dev@gmail.com>
2022-05-16 10:06:27 +0700

    + Setup Loader, Plugins

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-import helloWorld from './hello-world';
-import addImage from './add-image';
+import HelloWorldButton
+	from './components/hello-world-button/hello-world-button';
+import Heading from './components/heading/heading';
 
-helloWorld();
-addImage();
+const heading = new Heading();
+heading.render();
+const helloWorldButton = new HelloWorldButton();
+helloWorldButton.render();
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,16 @@
-const path = require('path');
+const path                 = require('path');
+const TerserPlugin         = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-	entry : './src/index.js',
-	output: {
+	entry  : './src/index.js',
+	output : {
 		filename  : 'bundle.js',
 		path      : path.resolve(__dirname, './dist'),
 		publicPath: 'dist/',
 	},
-	mode  : 'none',
-	module: {
+	mode   : 'none',
+	module : {
 		rules: [
 			{
 				test  : /\.(png|jpg)$/,
@@ -23,6 +25,31 @@ module.exports = {
 				test: /\.txt/,
 				type: 'asset/source',
 			},
+			{
+				test: /\.css$/,
+				use : [MiniCssExtractPlugin.loader, 'css-loader'],
+			},
+			{
+				test: /\.scss$/,
+				use : [MiniCssExtractPlugin.loader, 'css-loader', 'fast-sass-loader'],
+			},
+			{
+				test   : /\.js$/,
+				exclude: /node_modules/,
+				use    : {
+					loader : 'babel-loader',
+					options: {
+						presets: ['@babel/env'],
+						plugins: ['@babel/plugin-proposal-class-properties'],
+					},
+				},
+			},
 		],
 	},
+	plugins: [
+		new TerserPlugin(),
+		new MiniCssExtractPlugin({
+			filename: 'style.css',
+		}),
+	],
 };
