e635e4d4ffaac78f20dbb129c45f4bb99701da3b
SayudhaStudio <mazaya.dev@gmail.com>
2022-05-16 11:20:03 +0700

    + Setup Mode Development and Mode Production

--- webpack.development.config.js
+++ webpack.development.config.js
@@ -1,18 +1,26 @@
 const path                   = require('path');
-const TerserPlugin           = require('terser-webpack-plugin');
-const MiniCssExtractPlugin   = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin      = require('html-webpack-plugin');
 
 module.exports = {
-	entry  : './src/index.js',
-	output : {
-		filename  : 'bundle.[contenthash].js',
+	entry    : './src/index.js',
+	output   : {
+		filename  : 'bundle.js',
 		path      : path.resolve(__dirname, './dist'),
 		publicPath: '',
 	},
-	mode   : 'none',
-	module : {
+	mode     : 'development',
+	devServer: {
+		port         : 9000,
+		static       : {
+			directory: path.resolve(__dirname, './dist'),
+		},
+		devMiddleware: {
+			index      : 'index.html',
+			writeToDisk: true,
+		},
+	},
+	module   : {
 		rules: [
 			{
 				test  : /\.(png|jpg)$/,
@@ -29,11 +37,11 @@ module.exports = {
 			},
 			{
 				test: /\.css$/,
-				use : [MiniCssExtractPlugin.loader, 'css-loader'],
+				use : ['style-loader', 'css-loader'],
 			},
 			{
 				test: /\.scss$/,
-				use : [MiniCssExtractPlugin.loader, 'css-loader', 'fast-sass-loader'],
+				use : ['style-loader', 'css-loader', 'fast-sass-loader'],
 			},
 			{
 				test   : /\.js$/,
@@ -48,17 +56,13 @@ module.exports = {
 			},
 			{
 				test: /\.hbs$/,
-				use: [
-					'handlebars-loader'
-				]
-			}
+				use : [
+					'handlebars-loader',
+				],
+			},
 		],
 	},
-	plugins: [
-		new TerserPlugin(),
-		new MiniCssExtractPlugin({
-			filename: 'style.[contenthash].css',
-		}),
+	plugins  : [
 		new CleanWebpackPlugin({
 			cleanOnceBeforeBuildPatterns: [
 				'**/*',
---
+++ webpack.production.config.js
@@ -0,0 +1,72 @@
+const path                   = require('path');
+const MiniCssExtractPlugin   = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin      = require('html-webpack-plugin');
+
+module.exports = {
+	entry  : './src/index.js',
+	output : {
+		filename  : 'bundle.[contenthash].js',
+		path      : path.resolve(__dirname, './dist'),
+		publicPath: '',
+	},
+	mode   : 'production',
+	module : {
+		rules: [
+			{
+				test  : /\.(png|jpg)$/,
+				type  : 'asset',
+				parser: {
+					dataUrlCondition: {
+						maxSize: 3 * 1024, // 3 kilobytes
+					},
+				},
+			},
+			{
+				test: /\.txt/,
+				type: 'asset/source',
+			},
+			{
+				test: /\.css$/,
+				use : [MiniCssExtractPlugin.loader, 'css-loader'],
+			},
+			{
+				test: /\.scss$/,
+				use : [MiniCssExtractPlugin.loader, 'css-loader', 'fast-sass-loader'],
+			},
+			{
+				test   : /\.js$/,
+				exclude: /node_modules/,
+				use    : {
+					loader : 'babel-loader',
+					options: {
+						presets: ['@babel/env'],
+						plugins: ['@babel/plugin-proposal-class-properties'],
+					},
+				},
+			},
+			{
+				test: /\.hbs$/,
+				use : [
+					'handlebars-loader',
+				],
+			},
+		],
+	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: 'style.[contenthash].css',
+		}),
+		new CleanWebpackPlugin({
+			cleanOnceBeforeBuildPatterns: [
+				'**/*',
+				path.join(process.cwd(), 'build/**/*'),
+			],
+		}),
+		new HtmlWebpackPlugin({
+			title      : 'Hello world',
+			template   : 'src/index.hbs',
+			description: 'Some description',
+		}),
+	],
+};
