9f11fcfa74c8470f9d96af25ebb1f8b1b7e20e19
SayudhaStudio <mazaya.dev@gmail.com>
2022-05-16 10:52:59 +0700

    + Setup Handlebars and htmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,15 @@
-const path                 = require('path');
-const TerserPlugin         = require('terser-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path                   = require('path');
+const TerserPlugin           = require('terser-webpack-plugin');
+const MiniCssExtractPlugin   = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin      = require('html-webpack-plugin');
 
 module.exports = {
 	entry  : './src/index.js',
 	output : {
-		filename  : 'bundle.js',
+		filename  : 'bundle.[contenthash].js',
 		path      : path.resolve(__dirname, './dist'),
-		publicPath: 'dist/',
+		publicPath: '',
 	},
 	mode   : 'none',
 	module : {
@@ -44,12 +46,29 @@ module.exports = {
 					},
 				},
 			},
+			{
+				test: /\.hbs$/,
+				use: [
+					'handlebars-loader'
+				]
+			}
 		],
 	},
 	plugins: [
 		new TerserPlugin(),
 		new MiniCssExtractPlugin({
-			filename: 'style.css',
+			filename: 'style.[contenthash].css',
+		}),
+		new CleanWebpackPlugin({
+			cleanOnceBeforeBuildPatterns: [
+				'**/*',
+				path.join(process.cwd(), 'build/**/*'),
+			],
+		}),
+		new HtmlWebpackPlugin({
+			title      : 'Hello world',
+			template   : 'src/index.hbs',
+			description: 'Some description',
 		}),
 	],
 };
