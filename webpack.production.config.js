6b451ba27dbf2101f7af6f253a5f0b8f9c794a46
SayudhaStudio <mazaya.dev@gmail.com>
2022-05-21 09:54:33 +0700

    + Setup Pug Templating Environment

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,19 +1,46 @@
 const path                 = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin    = require('html-webpack-plugin');
 
 module.exports = {
-	entry : './src/index.js',
-	output: {
+	entry  : './src/index.js',
+	output : {
 		filename: 'bundle.[contenthash].js',
 		path    : path.resolve(__dirname, '/build'),
 	},
-	mode  : 'production',
-	module: {
+	mode   : 'production',
+	module : {
 		rules: [
 			{
 				test: /\.css$/,
 				use : [MiniCssExtractPlugin.loader, 'css-loader'],
 			},
+			{
+				test: /\.scss$/,
+				use : [MiniCssExtractPlugin.loader, 'css-loader', 'style-loader'],
+			},
+			{
+				test: /\.pug$/,
+				use : {
+					loader : 'pug-loader',
+					options: {
+						pretty: true,
+					},
+				},
+			},
 		],
 	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: 'style.[contenthash].css',
+		}),
+		new HtmlWebpackPlugin({
+			title      : 'Sayudha Project',
+			description: 'Sayudha Templating Single Page Environment Using Webpack',
+			template   : 'src/components/Templates/index.pug',
+			minify     : {
+				collapseWhitespace: false,
+			},
+		}),
+	],
 };
